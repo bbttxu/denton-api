3c918555595f3a42426aa83863d7ca9c0a1fd8c8
Adam Krawiec <adamkrawiec@gmail.com>
2014-06-28 10:56:11 -0500

    removing JSON response test

--- spec/functional/shows_controller_spec.rb
+++ spec/functional/shows_controller_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
-describe ShowsController do
-  describe "JSON interaction" do
-    it "should return JSON" do
-      request.accept = "application/json"
-      get :index
-      response.should be_success
-    end
-    it "should respond properly to a JSONP callback"
-  end
-end
+# describe ShowsController do
+#   describe "JSON interaction" do
+#     it "should return JSON" do
+#       request.accept = "application/json"
+#       get :index
+#       response.should be_success
+#     end
+#     it "should respond properly to a JSONP callback"
+#   end
+# end
