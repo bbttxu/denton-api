17b417384f9c9bca62716bca01b56f7412bae81e
Adam Krawiec <adamkrawiec@gmail.com>
2014-07-11 17:38:58 -0500

    testing day returns value, learning spec controller testing

--- spec/functional/shows_controller_spec.rb
+++ spec/functional/shows_controller_spec.rb
@@ -1,26 +1,34 @@
 require 'spec_helper'
 
 describe ShowsController, :type => :controller do
-  describe "JSON interaction" do
-    it "should return on index JSON" do
+  describe "an index" do
+    it "should return JSON" do
       request.accept = "application/json"
       get :index
       response.should be_success
     end
-    it "should respond properly to a JSONP callback"
 
-    it "should return calendar JSON" do
+    it "should respond properly to a JSONP callback"
+  end
+
+  describe "a calendar" do
+    it "should return JSON" do
       request.accept = "application/json"
       get :calendar
       response.should be_success
     end
+
     it "should respond properly to a JSONP callback"
 
-    it "should return shows JSON" do
+  end
+
+  describe "a given day" do
+    it "should return that days JSON" do
       request.accept = "application/json"
-      get "shows/2014-12-24"
+      get :day, date: "2014-12-14"
       response.should be_success
     end
+
     it "should respond properly to a JSONP callback"
   end
 end
