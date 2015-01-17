a007c1b551a5a57f0170f0cd7473ed854376c075
Adam Krawiec <adamkrawiec@gmail.com>
2015-01-16 18:18:05 -0600

    adding next show to venues

--- app/serializers/venue_serializer.rb
+++ app/serializers/venue_serializer.rb
@@ -1,10 +1,16 @@
 # venue_serializer.rb
 class VenueSerializer < ActiveModel::Serializer
-  attributes :name, :coordinates, :id, :slug, :shows_count
+  attributes :name, :coordinates, :id, :slug, :shows_count, :next_show
 
   has_many :shows, serializer: ShowSerializer, include: true, embed: :ids, key: :shows
 
   def shows_count
     object.shows.upcoming.size
+  end
+
+  def next_show
+    foo = object.shows.upcoming.first || nil
+    return foo['starts_at'] if foo
+    foo
   end
 end
